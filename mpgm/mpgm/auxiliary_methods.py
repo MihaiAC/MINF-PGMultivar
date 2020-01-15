Fix overflow issues, create an experiment framework.

mpgm/mpgm/auxiliary_methods.py
@@ -1,14 +1,14 @@
 import networkx as nx
 import numpy as np
 
+
 # TODO: pos_mean, neg_mean, std: temp variables;
-def generate_scale_free_graph(nr_variables, seed, weak_pos_mean, weak_neg_mean, weak_std, alpha, beta, gamma):
+def generate_scale_free_graph(nr_variables, weak_pos_mean, weak_neg_mean, weak_std, alpha, beta, gamma):
     # alpha: add new node + edge to existing node
     # beta: add new edge between existing node
     # gamma: add new node + edge from existing node
 
-
-    G = nx.scale_free_graph(nr_variables, alpha, beta, gamma, seed)
+    G = nx.scale_free_graph(nr_variables, alpha, beta, gamma, seed=np.random)
 
     # Remove self-loop edges.
     sle = list(nx.selfloop_edges(G))
@@ -28,3 +28,4 @@ def generate_scale_free_graph(nr_variables, seed, weak_pos_mean, weak_neg_mean, 
         G[u][v]['weight'] = weight
 
     return nx.to_numpy_array(G)
+

mpgm/mpgm/experiment_framework.py
@@ -1,8 +1,67 @@
-import networkx as nx
 import numpy as np
+import pickle
+import os
 from mpgm.mpgm.auxiliary_methods import generate_scale_free_graph
 from mpgm.mpgm.samplers import generate_samples_gibbs
 from mpgm.mpgm.tpgm import Tpgm
+from mpgm.mpgm.solvers import prox_grad
+
+
+class Experiment:
+    def __init__(self, graph_generator, graph_generator_params, generator_model, generator_model_params, sampler,
+                 sampler_params, model_to_fit, fit_params, random_seed, experiment_name, subexperiment_name,
+                 samples_not_generated_before=True):
+
+        self.graph_generator = graph_generator
+        self.graph_generator_params = graph_generator_params
+
+        self.generator_model = generator_model
+        self.generator_model_params = generator_model_params
+
+        self.sampler = sampler
+        self.sampler_params = sampler_params
+
+        self.model_to_fit = model_to_fit
+        self.fit_params = fit_params
+
+        self.experiment_name = experiment_name
+        self.subexperiment_name = subexperiment_name
+
+        self.random_seed = random_seed
+        np.random.seed(random_seed)
+
+        self.samples_not_generated_before = samples_not_generated_before
+
+    def run_experiment(self):
+        if self.samples_not_generated_before:
+            os.mkdir(self.experiment_name)
+        folder_path = self.experiment_name + '/'
+
+        # Save experiment parameters.
+        if self.samples_not_generated_before:
+            with open(folder_path + 'graph_sample_parameters.pkl', 'wb') as f:
+                pickle.dump(self.graph_generator.__name__, f)
+                pickle.dump(self.graph_generator_params, f)
+                pickle.dump(self.generator_model.__name__, f)
+                pickle.dump(self.generator_model_params, f)
+                pickle.dump(self.sampler.__name__, f)
+                pickle.dump(self.sampler_params, f)
+
+            generator_model_theta = self.graph_generator(*self.graph_generator_params)
+            np.save(folder_path + 'generator_model_theta.npy', generator_model_theta)
+            model = self.generator_model(generator_model_theta, *self.generator_model_params)
+            generated_data = self.sampler(model, *self.sampler_params)
+            np.save(folder_path + 'generated_data.npy', generated_data)
+
+        else:
+            generated_data = np.load(folder_path + 'generated_data.npy')
+
+        fit_data = self.model_to_fit.fit(generated_data, *self.fit_params)
+        with open(folder_path + self.subexperiment_name + '_fit_data.pkl', 'wb') as f:
+            pickle.dump(self.fit_params, f)
+            pickle.dump(fit_data, f)
+
+        print('Experiment ' + self.experiment_name + ', subexperiment ' + self.subexperiment_name + ' done!')
 
 # TODO: Add references to models' docstring.
 # TODO: Draw theta ss too for tpgm parameters.
@@ -11,35 +70,50 @@ from mpgm.mpgm.tpgm import Tpgm
 #   would be useful.
 # TODO: Make sure that np.seed is initialised only once for the whole program (HERE).
 
-
-
-def load_params_as_dict():
-    pass
-
-def save_model():
-    pass
-
 # TODO: Need to compare both graph structure and actual parameter values; recreate the graphs used by the papers.
-def test_tpgm(params_file_path, model_save_path):
 
-    # Placeholder for load_params:
+def test_tpgm():
+    random_seed = 200
+
     nr_variables = 50
-    seed = 200
-
     weak_pos_mean = 0.04
     weak_neg_mean = -0.04
-    weak_std = 0.03
+    weak_std = 0.3
+    alpha = 0.05
+    beta = 0.9
+    gamma = 0.05
+    graph_generator_params = (nr_variables, weak_pos_mean, weak_neg_mean, weak_std, alpha, beta, gamma)
+    graph_generator = generate_scale_free_graph
+    print(graph_generator.__name__)
 
-    np.random.seed(seed)
+    R = 50
+    generator_model = Tpgm
+    generator_model_params = [R]
 
-    alpha = 0.15
-    beta = 0.7
-    gamma = 0.15
+    sampler_init = np.random.randint(1, 3, nr_variables)
+    nr_samples = 200
+    burn_in = 100
+    thinning_nr = 70
+    sampler_params = (sampler_init, nr_samples, burn_in, thinning_nr)
+    sampler = generate_samples_gibbs
 
-    generate_scale_free_graph(nr_variables, seed, weak_pos_mean, weak_neg_mean, weak_std, alpha, beta, gamma)
+    theta_init = np.zeros(nr_variables)
+    alpha = np.sqrt(np.log(nr_variables)/nr_samples)
+    max_iter = 5000
+    max_line_search_iter = 50
+    lambda_p = 1.0
+    beta = 0.5
+    rel_tol = 1e-3
+    abs_tol = 1e-6
+    model_to_fit = Tpgm
+    fit_params = (theta_init, alpha, R, max_iter, max_line_search_iter, lambda_p, beta, rel_tol, abs_tol)
 
-    model = Tpgm(theta=A, seed=seed, R=100)
-    X = generate_samples_gibbs(model, np.random.randint(0, 3, A.shape[0]), nr_samples=200, burn_in=400, thinning_nr=70)
-    np.save('tpgm_sample1.npy', X)
+    experiment_name = 'TPGM_experiment_1'
+    subexperiment_name = 'run_2'
 
-    return
+    exp = Experiment(graph_generator, graph_generator_params, generator_model, generator_model_params, sampler,
+                     sampler_params, model_to_fit, fit_params, random_seed, experiment_name, subexperiment_name, False)
+    exp.run_experiment()
+
+
+#test_tpgm()

mpgm/mpgm/solvers.py
@@ -6,8 +6,8 @@ import tqdm
 # TODO: Check what to do with theta st and theta ts not agreeing. Can you do anything?
 # TODO: Check what to do with theta ss (node parameters). Include them too after testing?
 
-def prox_grad(theta_init, alpha, node, data, f, f_and_grad_f, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5,
-              rel_tol=1e-3, abs_tol=1e-6, *params_for_f_grad_f):
+def prox_grad(node, theta_init, alpha, data, f, f_and_grad_f, params_for_f_grad_f, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5,
+              rel_tol=1e-3, abs_tol=1e-6):
     # TODO: Add references.
     """
     Proximal gradient descent for solving the l1-regularized node-wise regressions required to fit some models in this
@@ -35,7 +35,7 @@ def prox_grad(theta_init, alpha, node, data, f, f_and_grad_f, max_iter=5000, max
     # TODO: Somehow test this method.
     # TODO: Make grad_f accept an optional parameter(partition).
 
-    likelihoods = np.zeros((1, max_line_search_iter))
+    likelihoods = np.zeros((max_iter, ))
     theta_k_2 = np.array(theta_init)
     theta_k_1 = np.array(theta_init)
     lambda_k = lambda_p
@@ -65,14 +65,15 @@ def prox_grad(theta_init, alpha, node, data, f, f_and_grad_f, max_iter=5000, max
             theta_k_1 = z
             likelihoods[k] = f_z
             # TODO: use decimal?
-            if (k > 0 and (np.abs(f_z - likelihoods[k - 1]) < abs_tol or
+            if (k > 2 and (np.abs(f_z - likelihoods[k - 1]) < abs_tol or
                            (np.abs(f_z - likelihoods[k - 1]) / min(np.abs(f_z),
                                                                    np.abs(likelihoods[k - 1]))) < rel_tol)):
                 return theta_k_1, likelihoods, True
         else:
             print('Line search failed to converge.')
-            return None
+            return theta_k_1, likelihoods, False
     return theta_k_1, likelihoods, False
+
 
 def soft_threshold(x, threshold):
     """

mpgm/mpgm/tpgm.py
@@ -1,8 +1,8 @@
 import numpy as np
-import tqdm
 from decimal import Decimal
 from multiprocessing import Pool
 from math import factorial
+from mpgm.mpgm.solvers import prox_grad
 
 
 class Tpgm(object):
@@ -14,15 +14,13 @@ class Tpgm(object):
     :param R: maximum count (truncation value).
     :param log_factorials: Log factorials of integers up to R.
     :param data: Data to fit the model with.
-    :param seed: Seed used for np.random.
     """
 
-    def __init__(self, theta=None, data=None, R=100):
+    def __init__(self, theta=None, R=100):
         """
         Constructor for TPGM class.
 
         :param theta: N x N matrix, given when we want to generate samples from an existing distribution.
-        :param data: M x N matrix, one row is equivalent to one sample of the distribution to be fitted.
         :param R: maximum count value, should be an integer.
         """
 
@@ -33,11 +31,6 @@ class Tpgm(object):
         if theta is not None:
             self.N = len(theta[0])
             self.theta = np.array(theta)
-
-        if data is not None:
-            self.data = np.array(data)
-            self.data[self.data >= self.R] = self.R
-            self.N = len(data[0])
 
     def __setattr__(self, key, value):
         if key == 'R':
@@ -59,19 +52,17 @@ class Tpgm(object):
         uu = np.random.uniform(0, 1, 1)[0]
         uu = Decimal(uu)
 
-        prob1, partition, dot_product = self.node_cond_prob(node, 0, data)
+        prob1, partition_max_exp, partition_reduced, dot_product = self.node_cond_prob(node, 0, data)
         cdf = prob1
 
         for node_value in range(1, self.R + 1):
-            #print(type(uu))
-            #print(type(cdf))
             if uu.compare(Decimal(cdf)) == Decimal('-1'):
                 return node_value-1
-            cdf += self.node_cond_prob(node, node_value, data, partition, dot_product)[0]
+            cdf += self.node_cond_prob(node, node_value, data, dot_product, partition_max_exp, partition_reduced)[0]
 
         return self.R
 
-    def node_cond_prob(self, node, node_value, data, partition=None, dot_product=None):
+    def node_cond_prob(self, node, node_value, data, dot_product=None, partition_max_exp=None, partition_reduced=None):
         """
         Calculates the probability of node having the provided value given the other nodes.
 
@@ -88,14 +79,21 @@ class Tpgm(object):
 
         #TODO: OPTIMIZE PARTITION SO IT WON'T OVERFLOW!!!!!!!!!!!!!!!! (scoate factor comun exp(cel mai mare exponent))
         #   si imparte pe rand!!!!!!!!!!!!!!!!!
-        if partition is None:
-            partition = 0
+        if partition_reduced is None:
+
+            partition_exponents = np.zeros((self.R+1, ))
             for kk in range(self.R+1):
-                partition += np.exp(dot_product * kk - self.log_factorials[kk])
+                partition_exponents[kk] = dot_product * kk - self.log_factorials[kk]
 
-        cond_prob = np.exp(dot_product * node_value - self.log_factorials[node_value])/partition
+            partition_max_exp = max(partition_exponents)
+            partition_reduced = 0
+            for kk in range(self.R+1):
+                partition_reduced += np.exp(partition_exponents[kk] - partition_max_exp)
 
-        return cond_prob, partition, dot_product
+
+        cond_prob = np.exp(dot_product * node_value - self.log_factorials[node_value] - partition_max_exp)/partition_reduced
+
+        return cond_prob, partition_max_exp, partition_reduced, dot_product
 
     @staticmethod
     def calculate_log_factorials_upto(R):
@@ -116,7 +114,6 @@ class Tpgm(object):
 
         return log_factorials
 
-
     @staticmethod
     def calculate_ll_datapoint(node, datapoint, theta, R):
         """
@@ -128,7 +125,7 @@ class Tpgm(object):
         exponents = []
         dot_product = np.dot(datapoint, theta) - theta[node] * datapoint[node] + theta[node]
         for kk in range(R+1):
-            curr_exponent = dot_product * kk - np.log(factorial(kk))
+            curr_exponent = dot_product * kk - np.log(float(factorial(kk)))
             exponents.append(curr_exponent)
 
         max_exponent = max(exponents)
@@ -140,7 +137,7 @@ class Tpgm(object):
 
         log_partition += np.log(sum_of_rest)
 
-        return dot_product * datapoint[node] - np.log(factorial(datapoint[node])) - log_partition, log_partition
+        return dot_product * datapoint[node] - np.log(float(factorial(datapoint[node]))) - log_partition, log_partition
 
     @staticmethod
     def calculate_grad_ll_datapoint(node, datapoint, theta, R, log_partition):
@@ -149,8 +146,8 @@ class Tpgm(object):
         dot_product = np.dot(datapoint, theta) - theta[node] * datapoint[node] + theta[node]
 
         log_partition_derivative_term = 0
-        for kk in range(R+1):
-            log_partition_derivative_term += np.exp(dot_product * kk - np.log(factorial(kk)) + np.log(kk) - log_partition)
+        for kk in range(1, R+1):
+            log_partition_derivative_term += np.exp(dot_product * kk - np.log(float(factorial(kk))) + np.log(kk) - log_partition)
 
         grad[node] = datapoint[node] - log_partition_derivative_term
         for ii in range(datapoint.shape[0]):
@@ -190,18 +187,21 @@ class Tpgm(object):
         nll = 0
 
         for ii in range(N):
-            nll -= Tpgm.calculate_ll_datapoint(node, data[ii, :], theta, R)
+            nll -= Tpgm.calculate_ll_datapoint(node, data[ii, :], theta, R)[0]
 
         nll = nll/N
 
         return nll
 
-
-
-
+    @staticmethod
+    def provide_args(nr_nodes, other_args):
+        for ii in range(nr_nodes):
+            yield [ii] + other_args
+        return
 
     # TODO: Add docstring + smart kwargs; parallelize for each node with multiprocessing.
-    def fit(self, data, theta_init, alpha, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5,
+    @staticmethod
+    def fit(data, theta_init, alpha, R, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5,
             rel_tol=1e-3, abs_tol=1e-6):
         """
         :param data: N X P matrix; each row is a datapoint;
@@ -216,5 +216,14 @@ class Tpgm(object):
         :return:
         """
 
-        f = self.nll_node_cond_prob
-        grad_f = self.nll_derivative_node_cond_prob
+        f = Tpgm.calculate_nll
+        f_and_grad_f = Tpgm.calculate_nll_and_grad_nll
+
+        tail_args = [theta_init, alpha, data, f, f_and_grad_f, [R], max_iter, max_line_search_iter, lambda_p, beta,
+                     rel_tol, abs_tol]
+        nr_nodes = data.shape[1]
+
+        with Pool(processes=4) as pool:
+            return pool.starmap(prox_grad, Tpgm.provide_args(nr_nodes, tail_args))
+
+