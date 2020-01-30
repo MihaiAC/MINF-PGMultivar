Further simplify experiment_framework, samplers; fix some lpgm bugs having to do with wrong dimensionalities. Fix some bugs in prox_grad having to do with wrong array indices.

mpgm/mpgm/LPGM.py
@@ -32,7 +32,7 @@ class LPGM(Model):
         dots_Wi_beta = np.dot(data, theta_curr)
 
         nll = -np.dot(Y, dots_Wi_beta) + np.sum(np.exp(dots_Wi_beta))
-        grad_nll = -np.sum(W * (Y - np.exp(dots_Wi_beta)), axis=0)
+        grad_nll = -np.dot(Y - np.exp(dots_Wi_beta), W)
 
         nll = (nll/N) - 1 # exp(W[ii, node] * theta[node]) = exp(0) = 1; removing this.
         grad_nll = grad_nll/N
@@ -75,24 +75,26 @@ class LPGM(Model):
         prox_grad_unmutable_params = [max_iter, max_line_search_iter, lambda_p, beta, rel_tol, abs_tol]
 
         thetas_main = np.zeros((len(alpha_values), data.shape[1]))
-        thetas_subsamples = np.zeros(self.lpgm_B, len(alpha_values), data.shape[1])
+        thetas_subsamples = np.zeros((self.lpgm_B, len(alpha_values), data.shape[1]))
 
         local_rng = np.random.RandomState(self.seeds[node])
 
         theta_init = local_rng.normal(0, 0.0001, data.shape[1])
+        self.theta = theta_init
         for ii, alpha in enumerate(alpha_values):
-            theta_warm = prox_grad(node, theta_init, alpha, data, *prox_grad_unmutable_params)[0]
+            theta_warm = prox_grad(node, self, data, alpha, *prox_grad_unmutable_params)[0]
             thetas_main[ii, :] = theta_warm
-            theta_init = theta_warm
+            self.theta = theta_warm
 
         for bb in range(self.lpgm_B):
             theta_init = local_rng.normal(0, 0.0001, data.shape[1])
+            self.theta = theta_init
             subsample_indices = local_rng.choice(data.shape[0], self.lpgm_m, replace=False)
             subsampled_data = data[subsample_indices, :]
             for ii, alpha in enumerate(alpha_values):
-                theta_warm = prox_grad(node, theta_init, alpha, subsampled_data, *prox_grad_unmutable_params)[0]
+                theta_warm = prox_grad(node, self, subsampled_data, alpha, *prox_grad_unmutable_params)[0]
                 thetas_subsamples[bb, ii, :] = theta_warm
-                theta_init = theta_warm
+                self.theta = theta_warm
 
         return thetas_main, thetas_subsamples
 

mpgm/mpgm/experiment_framework.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pickle
 import os
-from mpgm.mpgm.samplers import Sampler
 from mpgm.mpgm.TPGM import TPGM
 from mpgm.mpgm.QPGM import QPGM
 from mpgm.mpgm.SPGM import SPGM
 from mpgm.mpgm.LPGM import LPGM
-generate_gibbs_samples = Sampler.generate_samples_gibbs()
 
 class Experiment:
-    def __init__(self, data_file_path, model_to_fit_name, experiment_name, subexperiment_name, random_seed=200, R=10,
+    def __init__(self, samples_name, model_to_fit_name, experiment_name, random_seed=200, R=10,
                  R0 = 5, theta_init=None, theta_quad_init=None, alpha=None, lpgm_m=None, lpgm_B = 10, lpgm_beta=0.05,
                  nr_alphas=100, max_iter=1000, max_line_search_iter=100, prox_grad_lambda_p=1.0, prox_grad_beta=0.5,
                  rel_tol=1e-6, abs_tol=1e-9):
@@ -21,13 +19,12 @@ class Experiment:
         :param experiment_name: Name of the experiment. Two experiments are different if they use different sampled data.
         :param subexperiment_name: Subexperiments within an experiment.
         '''
-        self.data_file_path = data_file_path
+        self.data_file_path = 'Samples/' + samples_name + '/'
         self.experiment_name = experiment_name
-        self.subexperiment_name = subexperiment_name
 
         np.random.seed(random_seed)
 
-        self.data = np.load(self.data_file_path + '/generated_samples.npy')
+        self.data = np.load(self.data_file_path + 'samples.npy')
         nr_samples, nr_variables = self.data.shape
 
         if(theta_init == None):
@@ -43,6 +40,7 @@ class Experiment:
                                  'prox_grad_lambda_p': prox_grad_lambda_p, 'prox_grad_beta': prox_grad_beta,
                                  'rel_tol': rel_tol, 'abs_tol': abs_tol})
 
+        self.model_to_fit_name = model_to_fit_name
         if(model_to_fit_name == 'TPGM'):
             self.model_to_fit = TPGM(theta_init, R)
         elif(model_to_fit_name == 'QPGM'):
@@ -57,18 +55,65 @@ class Experiment:
             self.model_to_fit = LPGM(lpgm_m, lpgm_B, lpgm_beta, nr_alphas, seeds)
 
         self.experiment_name = experiment_name
-        self.subexperiment_name = subexperiment_name
 
     def run_experiment(self):
-        if(not os.path.isdir(self.experiment_name)):
-            os.mkdir(self.experiment_name)
-
-        folder_path = self.experiment_name + '/'
-
         fit_data = self.model_to_fit.fit(self.data, **self.prox_grad_params)
-        with open(folder_path + self.subexperiment_name + '_fit_data.pkl', 'wb') as f:
-            pickle.dump(self.model_to_fit, f)
-            pickle.dump(self.prox_grad_params, f)
-            pickle.dump(fit_data, f)
 
-        print('Experiment ' + self.experiment_name + ', subexperiment ' + self.subexperiment_name + ' done!')
+        experiment_folder = self.data_file_path + self.experiment_name
+        if(not os.path.isdir(experiment_folder)):
+            os.mkdir(experiment_folder)
+        experiment_folder = experiment_folder + '/'
+
+        self.save_experiment(experiment_folder, fit_data)
+
+        print('Experiment ' + self.experiment_name + ' done!')
+
+    def save_experiment(self, experiment_folder, fit_data):
+        if(self.model_to_fit_name in ['TPGM', 'SPGM', 'QPGM']):
+            fit_model_theta, likelihoods, conditions, converged = self.reshape_fit_data_TQSPGM(fit_data)
+            np.save(experiment_folder + 'fit_model_theta.npy', fit_model_theta)
+
+            with open(experiment_folder + 'likelihoods.pkl', 'wb') as f:
+                pickle.dump(likelihoods, f)
+
+            with open(experiment_folder + 'converged.pkl', 'wb') as f:
+                pickle.dump(converged, f)
+
+            if(self.model_to_fit_name == 'TPGM'):
+                return
+
+            with open(experiment_folder + 'conditions.pkl', 'wb') as f:
+                pickle.dump(conditions, f)
+
+        elif(self.model_to_fit_name == 'LPGM'):
+            np.save(experiment_folder + 'optimal_theta.npy', np.array([fit_data[0]]))
+            np.save(experiment_folder + 'fit_model_theta.npy', fit_data[1])
+
+        else:
+            print('Invalid value for model_to_fit_name.')
+
+    def reshape_fit_data_TQSPGM(self, fit_data):
+        nr_variables = len(fit_data)
+
+        fit_model_theta = np.zeros((nr_variables, nr_variables))
+        likelihoods = []
+        conditions = []
+        converged = []
+
+        for ii in range(len(fit_data)):
+            fit_model_theta[ii, :] = fit_data[ii][0]
+            likelihoods.append(fit_data[ii][1])
+            conditions.append(fit_data[ii][2])
+            converged.append(fit_data[ii][3])
+
+        return fit_model_theta, likelihoods, conditions, converged
+
+def run_test_experiment():
+    exp = Experiment('test', 'TPGM', 'TPGM_fit_test')
+    exp.run_experiment()
+
+def run_test_lpgm_experiment():
+    exp = Experiment('test', 'LPGM', 'LPGM_fit_test', lpgm_m=10)
+    exp.run_experiment()
+
+run_test_lpgm_experiment()

mpgm/mpgm/samplers.py
@@ -6,6 +6,7 @@ from mpgm.mpgm.TPGM import TPGM
 from mpgm.mpgm.QPGM import QPGM
 from mpgm.mpgm.SPGM import SPGM
 from mpgm.mpgm.auxiliary_methods import generate_scale_free_graph
+from pathlib import Path
 
 class Sampler():
     def __init__(self, graph_generator, graph_generator_params, generator_model, generator_model_params, sampling_method,
@@ -31,29 +32,35 @@ class Sampler():
     def generate_samples_to_folder(self, folder_path):
         if os.path.isdir(folder_path):
             print('The sample parameters and samples in ' + folder_path + ' will be overwritten. Type Y to proceed, ' +
-                                                                          + 'anything else to abort.')
+                  'anything else to abort.')
             confirm = input()
             if confirm == "Y" or confirm == 'y':
                 print('Overwrite confirmed.')
+            else:
+                return
         else:
-            os.mkdir(folder_path)
+            Path(folder_path).mkdir(parents=True, exist_ok=True)
 
         folder_path = folder_path + '/'
+        generator_model_theta = self.graph_generator(**self.graph_generator_params)
+        model = self.generator_model(generator_model_theta, **self.generator_model_params)
+        generated_samples = self.sampling_method(model, **self.sampling_method_params)
 
         # Save the parameters corresponding to the sample we've generated:
-        with open(folder_path + 'graph_sample_parameters.pkl', 'wb') as f:
+        with open(folder_path + 'graph_generator_params.pkl', 'wb') as f:
             pickle.dump(self.graph_generator.__name__, f)
             pickle.dump(self.graph_generator_params, f)
+
+        with open(folder_path + 'generator_model_params.pkl', 'wb') as f:
             pickle.dump(self.generator_model.__name__, f)
             pickle.dump(self.generator_model_params, f)
+
+        with open(folder_path + 'sampling_method_params.pkl', 'wb') as f:
             pickle.dump(self.sampling_method.__name__, f)
             pickle.dump(self.sampling_method_params, f)
 
-        generator_model_theta = self.graph_generator(**self.graph_generator_params)
         np.save(folder_path + 'generator_model_theta.npy', generator_model_theta)
-        model = self.generator_model(generator_model_theta, **self.generator_model_params)
-        generated_samples = self.sampling_method(model, **self.sampling_method_params)
-        np.save(folder_path + 'generated_samples.npy', generated_samples)
+        np.save(folder_path + 'samples.npy', generated_samples)
 
     @staticmethod
     def generate_samples_gibbs(model, init, nr_samples=50, burn_in=700, thinning_nr=100):
@@ -61,14 +68,14 @@ class Sampler():
         Gibbs sampler for this implementation of the TPGM distribution.
 
         :param model: model to draw samples from, must implement "generate_sample_cond_prob(node, data)"
-        :param init: 1xN np array = starting value for the sampling process;
+        :param init: (N, ) np array = starting value for the sampling process;
         :param nr_samples: number of samples we want to generate.
         :param burn_in: number of initial samples to be discarded.
         :param thinning_nr: we select every thinning_nr samples after the burn-in period;
 
         :return: (nr_samples X N) matrix containing one sample per row.
         """
-        N = np.shape(init)[0]
+        N = len(init)
         samples = np.zeros((nr_samples, N))
         nodes_values = np.array(init)
 
@@ -85,11 +92,14 @@ class Sampler():
         return samples
 
 class SamplerWrapper():
-    def __init__(self, graph_generator_name, generator_model_name, sampling_method_name, samples_id, random_seed=200,
-                 nr_variables=10, weak_pos_mean=0.04, weak_neg_mean=-0.04, weak_std=0.3, scale_free_alpha=0.1,
+    def __init__(self, graph_generator_name, generator_model_name, sampling_method_name, samples_name, random_seed=200,
+                 nr_variables=10, weak_pos_mean=0.04, weak_neg_mean=-0.04, weak_std=0.03, scale_free_alpha=0.1,
                  scale_free_beta=0.8, scale_free_gamma=0.1, R=10, R0=5, nr_samples=50, burn_in=200, thinning_nr=50,
                  theta_quad=None, sampler_init_values=None):
         np.random.seed(random_seed)
+
+        if(sampler_init_values == None):
+            sampler_init_values = np.random.randint(0, 3, (nr_variables, ))
 
         if(graph_generator_name == 'scale_free'):
             self.graph_generator = generate_scale_free_graph
@@ -119,11 +129,22 @@ class SamplerWrapper():
         else:
             raise ValueError('Invalid sampling method name.')
 
-        self.samples_folder = 'Samples/' + generator_model_name + '_' + graph_generator_name + '/Samples_' + \
-                              str(samples_id)
+        self.samples_folder = 'Samples/' + samples_name
 
     def generate_samples(self):
         sampler = Sampler(self.graph_generator, self.graph_generator_params, self.generator_model,
                           self.generator_model_params, self.sampling_method, self.sampling_method_params)
         sampler.generate_samples_to_folder(self.samples_folder)
 
+
+def generate_TPGM_samples1():
+    sampler_wrapper = SamplerWrapper('scale_free', 'TPGM', 'gibbs', 'test', random_seed=145, weak_std=0.03,
+                                     nr_samples=30, burn_in=200, thinning_nr=300, nr_variables=10, R=10)
+    sampler_wrapper.generate_samples()
+
+def generate_TPGM_samples_higher_thinning_nr():
+    sampler_wrapper = SamplerWrapper('scale_free', 'TPGM', 'gibbs', 'test', random_seed=145, weak_std=0.03,
+                                     nr_samples=30, burn_in=200, thinning_nr=1000, nr_variables=10, R=10)
+    sampler_wrapper.generate_samples()
+
+generate_TPGM_samples1()

mpgm/mpgm/solvers.py
@@ -59,20 +59,18 @@ def prox_grad(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, 
         if sw:
             theta_k_2 = theta_k_1
             theta_k_1 = z
-            likelihoods[k] = f_z
+            likelihoods[k-1] = f_z
 
             if condition is not None:
-                conditions[k] = condition(node, z, data)
+                conditions[k-1] = condition(node, z, data)
 
-            if (k > 2 and (np.abs(f_z - likelihoods[k - 1]) < abs_tol or
-                           (np.abs(f_z - likelihoods[k - 1]) / min(np.abs(f_z),
-                                                                   np.abs(likelihoods[k - 1]))) < rel_tol)):
+            if (k > 2 and (np.abs(f_z - likelihoods[k - 2]) < abs_tol or
+                           (np.abs(f_z - likelihoods[k - 2]) / min(np.abs(f_z),
+                                                                   np.abs(likelihoods[k - 2]))) < rel_tol)):
                 converged = True
                 break
         else:
-            print('Line search failed to converge for node ' + str(node))
             break
-    print('prox_grad for node ' + str(node) + ' has finished. Converged?: ' + str(converged))
     return theta_k_1, likelihoods, conditions, converged
 
 