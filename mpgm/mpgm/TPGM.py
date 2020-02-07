Add gradient descent and unaccelerated proximal gradient descent to solvers. Solve bug in calculating the gradient of the log-likelihood in TPGM. Test proximal gradient descent method and TPGM's sampler and log-likelihood-calculating methods.

mpgm/mpgm/TPGM.py
@@ -1,6 +1,6 @@
 import numpy as np
 from multiprocessing import Pool
-from mpgm.mpgm.solvers import prox_grad
+from mpgm.mpgm.solvers import prox_grad, grad_descent
 from mpgm.mpgm.model import Model
 from scipy.special import gammaln
 
@@ -78,6 +78,7 @@ class TPGM(Model):
             for kk in range(self.R+1):
                 partition_reduced += np.exp(partition_exponents[kk] - partition_max_exp)
 
+        min_exp = dot_product * node_value - gammaln(node_value+1)
         cond_prob = np.exp(dot_product * node_value - gammaln(node_value+1) - partition_max_exp)/partition_reduced
 
         return cond_prob, partition_max_exp, partition_reduced, dot_product
@@ -104,7 +105,9 @@ class TPGM(Model):
 
         log_partition += np.log(sum_of_rest)
 
-        return dot_product * datapoint[node] - gammaln(datapoint[node] + 1) - log_partition, log_partition
+        # TODO: remove debug statement.
+        ll = dot_product * datapoint[node] - gammaln(datapoint[node] + 1) - log_partition
+        return ll, log_partition
 
     def calculate_grad_ll_datapoint(self, node, datapoint, theta_curr, log_partition):
         grad = np.zeros(datapoint.shape)
@@ -116,6 +119,7 @@ class TPGM(Model):
         #    log_partition_derivative_term += np.exp(dot_product * kk - gammaln(kk+1) + np.log(kk) - log_partition)
         exponents_numerator = []
         exponents_denominator = []
+        exponents_denominator.append(-gammaln(1))
         for kk in range(1, self.R+1):
             exponents_numerator.append(dot_product * kk - gammaln(kk+1) + np.log(kk))
             exponents_denominator.append(dot_product * kk - gammaln(kk+1))
@@ -128,6 +132,7 @@ class TPGM(Model):
         for ii in range(self.R):
             sum_numerator += np.exp(exponents_numerator[ii] - max_exponent_numerator)
             sum_denominator += np.exp(exponents_denominator[ii] - max_exponent_denominator)
+        sum_denominator += np.exp(exponents_denominator[self.R] - max_exponent_denominator)
 
         log_partition_derivative_term = np.exp(max_exponent_numerator - max_exponent_denominator) * \
                                         (sum_numerator/sum_denominator)
@@ -165,9 +170,12 @@ class TPGM(Model):
                      abs_tol]
         nr_nodes = data.shape[1]
 
-        return [prox_grad(0, *tail_args)]
+        with Pool(processes=4) as pool:
+            args = list(Model.provide_args(nr_nodes, tail_args))
+            return pool.starmap(grad_descent, args)
 
-        #with Pool(processes=4) as pool:
-        #    args = list(Model.provide_args(nr_nodes, tail_args))
-        #    return pool.starmap(prox_grad, args)
-
+if __name__ == '__main__':
+    data = np.load('Samples/test/samples.npy')
+    alpha = 0.1
+    tpgm = TPGM(theta=np.zeros((10, )), R=10)
+    grad_descent(0, tpgm, data, alpha)

mpgm/mpgm/analysis_tools.py
@@ -11,7 +11,7 @@ def calculate_tpr_fpr(theta_orig, theta_fit):
     for ii in range(1, nr_variables):
         for kk in range(ii):
             real_edge = theta_orig[ii][kk] != 0
-            inferred_edge = abs(theta_fit[ii][kk]) >= 1e-4 or abs(theta_fit[kk][ii]) >= 1e-4
+            inferred_edge = abs(theta_fit[ii][kk]) >= 1e-1 or abs(theta_fit[kk][ii]) >= 1e-1
 
             if (real_edge and inferred_edge):
                 TP += 1
@@ -29,10 +29,10 @@ def calculate_tpr_fpr(theta_orig, theta_fit):
 samples = np.load('Samples/test/samples.npy')
 with open('Samples/test/TPGM_fit_test/likelihoods.pkl', 'rb') as f:
     likelihoods = pkl.load(f)
-print(likelihoods[0])
-print(likelihoods[1])
+
 theta_orig = np.load('Samples/test/generator_model_theta.npy')
-theta_fit = np.load('Samples/test/TPGM_fit_test/fit_model_theta.npy')
+theta_fit = np.load('Samples/test/LPGM_fit_test/fit_model_theta.npy')
+
 print(theta_orig)
 print(theta_fit)
 print(calculate_tpr_fpr(theta_orig, theta_fit))

mpgm/mpgm/experiment_framework.py
@@ -109,11 +109,11 @@ class Experiment:
         return fit_model_theta, likelihoods, conditions, converged
 
 def run_test_experiment():
-    exp = Experiment('test', 'TPGM', 'TPGM_fit_test', random_seed=1337)
+    exp = Experiment('test', 'TPGM', 'TPGM_fit_test', alpha=0.1, theta_init=np.random.uniform(0, 1/(10 * 10), (10, )), random_seed=1337)
     exp.run_experiment()
 
 def run_test_lpgm_experiment():
-    exp = Experiment('test', 'LPGM', 'LPGM_fit_test', lpgm_m=10)
+    exp = Experiment('test', 'LPGM', 'LPGM_fit_test', lpgm_m=24)
     exp.run_experiment()
 
-run_test_experiment()
+run_test_lpgm_experiment()

mpgm/mpgm/solvers.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sys
 
 def prox_grad(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5, rel_tol=1e-3,
               abs_tol=1e-6):
@@ -38,6 +38,8 @@ def prox_grad(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, 
 
     converged = False
 
+    epsilon = sys.float_info.epsilon
+
     z = np.zeros(np.size(theta_init))
     f_z = 0
 
@@ -45,29 +47,18 @@ def prox_grad(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, 
         w_k = k / (k + 3)
         y_k = theta_k_1 + w_k * (theta_k_1 - theta_k_2)
         f_y_k, grad_y_k = f_and_grad_f(node, data, y_k)
-        #print(theta_k_1)
 
         sw = False
         for _ in range(max_line_search_iter):
             z = soft_threshold(y_k - lambda_k * grad_y_k, threshold=lambda_k * alpha)
-            #print(z)
-            f_tilde = f_y_k + np.dot(grad_y_k, z - y_k) + (1 / (2 * lambda_k)) * (np.linalg.norm(z - y_k) ** 2)
-            #print(f_tilde)
-            #print('------------------------')
+            f_tilde = f_y_k + np.dot(grad_y_k, z - y_k) + (1 / (2 * lambda_k)) * np.sum((z - y_k) ** 2)
             f_z = f(node, data, z)  # NLL at current step.
-            #print(f_z)
-            if f_z <= f_tilde:
+            if f_z < f_tilde or np.isclose(f_z, f_tilde, rtol=1e-3):
                 sw = True
                 break
             else:
                 lambda_k = lambda_k * beta
         if sw:
-            #print(theta_k_1)
-            #print(theta_k_2)
-            #print(z)
-            #print(lambda_k)
-            #print('----------------------')
-            print(z)
             theta_k_2 = theta_k_1
             theta_k_1 = z
 
@@ -76,14 +67,13 @@ def prox_grad(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, 
             if condition is not None:
                 conditions[k-1] = condition(node, z, data)
 
-            if (k > 2 and (np.abs(f_z - likelihoods[k - 2]) < abs_tol or
-                           (np.abs(f_z - likelihoods[k - 2]) / min(np.abs(f_z),
-                                                                   np.abs(likelihoods[k - 2]))) < rel_tol)):
-                converged = True
-                break
+            #if (k > 3 and (np.abs(f_z - likelihoods[k - 2]) < abs_tol or
+            #               (np.abs(f_z - likelihoods[k - 2]) / min(np.abs(f_z),
+            #                                                       np.abs(likelihoods[k - 2]))) < rel_tol)):
+            #    converged = True
+            #    break
         else:
             break
-    print(likelihoods[0:10])
     return theta_k_1, likelihoods, conditions, converged
 
 
@@ -98,3 +88,63 @@ def soft_threshold(x, threshold):
     x_plus = x - threshold
     x_minus = -x - threshold
     return x_plus * (x_plus > 0) - x_minus * (x_minus > 0)
+
+
+class stupid_model():
+    def __init__(self, data, theta):
+        self.data = data
+        self.theta = theta
+        self.condition = None
+
+    def calculate_nll(self, node, data, theta):
+        nll = 0
+        for ii in range(len(data)):
+            nll += np.exp(theta[ii] * data[ii])
+        return nll
+
+    def calculate_nll_and_grad_nll(self, node, data, theta):
+        grad = np.zeros(data.shape)
+        for ii in range(len(data)):
+            grad[ii] = np.exp(data[ii] * theta[ii]) * theta[ii]
+        return self.calculate_nll(node, data, theta), -grad
+
+def grad_descent(node, model, data, alpha, max_iter=5000, max_line_search_iter=50, lambda_p=1.0, beta=0.5, rel_tol=1e-3,
+              abs_tol=1e-6):
+
+    condition = model.condition
+    f = model.calculate_nll
+    f_and_grad_f = model.calculate_nll_and_grad_nll
+    theta_init = model.theta
+
+    likelihoods = np.zeros((max_iter, ))
+
+    conditions = None
+    if condition is not None:
+        conditions = np.zeros((max_iter, ))
+
+    y_k = np.array(theta_init)
+    lambda_k = lambda_p
+
+    converged = False
+
+    for k in range(1, max_iter + 1):
+        f_y_k, grad_y_k = f_and_grad_f(node, data, y_k)
+        likelihoods[k-1] = f_y_k
+        y_k = y_k - alpha * grad_y_k
+
+        if condition is not None:
+            conditions[k-1] = condition(node, z, data)
+
+            #if (k > 3 and (np.abs(f_z - likelihoods[k - 2]) < abs_tol or
+            #               (np.abs(f_z - likelihoods[k - 2]) / min(np.abs(f_z),
+            #                                                       np.abs(likelihoods[k - 2]))) < rel_tol)):
+            #    converged = True
+            #    break
+    return y_k, likelihoods, conditions, converged
+
+if __name__ == '__main__':
+    stupid = stupid_model(None, np.ones(3))
+    theta_final, likelihoods, _, _ = grad_descent(0, stupid, np.array([-2, -4, -5]), 0.1, max_iter=10000)
+    print(likelihoods[:100])
+    print(likelihoods[-100:])
+    print(theta_final)