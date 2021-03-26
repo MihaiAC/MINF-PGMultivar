cbe39ec2105979dbee91d9037074dd83fdcb7b3c
MihaiAC <alexmihai.ciobanu@gmail.com>
2021-03-26 00:11:15 +0000

    Push PGM and minor changes to SPGM.

--- mpgm/mpgm/models/PGM.py
+++ mpgm/mpgm/models/PGM.py
@@ -1,42 +1,24 @@
 import numpy as np
-from tqdm import tqdm
-from multiprocessing import Pool
 from scipy.special import gammaln, logsumexp
-from scipy.optimize import minimize
 from mpgm.mpgm.models.Model import Model
-from functools import partial
 
 from typing import Optional, List, Tuple
 
 
-class SPGM(Model):
-    def __init__(self, theta:Optional[np.ndarray]=None, R:Optional[int]=10, R0:Optional[int]=5,
-                 partition_atol:Optional[float]=1e-20, partition_max_iter:Optional[int]=10000):
+class PGM(Model):
+    def __init__(self,
+                 theta:Optional[np.ndarray]=None,
+                 partition_atol:Optional[float]=1e-20,
+                 partition_max_iter:Optional[int]=10000):
         super().__init__(theta)
-        self.R = R
-        self.R0 = R0
 
         self.partition_atol = partition_atol
         self.partition_max_iter = partition_max_iter
 
         self.condition = None
 
-    def __setattr__(self, key, value):
-        if key == 'R':
-            assert type(value) is int and value > 0, "R must be a positive integer"
-
-        if key == 'R0':
-            assert type(value) is int and value > 0 and value <= self.R, 'R0 must be an integer which satisfies 0 < R0 < R'
-
-        super(SPGM, self).__setattr__(key, value)
-
-    def sufficient_statistics(self, node_value):
-        if(node_value <= self.R0):
-            return node_value
-        elif(node_value <= self.R):
-            return (-0.5 * (node_value ** 2) + self.R * node_value - 0.5 * (self.R0 ** 2)) / (self.R - self.R0)
-        else:
-            return 0.5 * (self.R + self.R0)
+    def sufficient_statistics(self, node):
+        return node
 
     def estimate_partition_exponents(self, node:int, node_values_suffst:np.ndarray) -> List[float]:
         '''
@@ -59,7 +41,11 @@ class SPGM(Model):
                     gammaln(node_value+1)
         return exponents
 
-    def node_cond_prob(self, node:int, node_value:int, node_values_suffst:np.ndarray, dot_product:Optional[float]=None,
+    def node_cond_prob(self,
+                       node:int,
+                       node_value:int,
+                       node_values_suffst:np.ndarray,
+                       dot_product:Optional[float]=None,
                        log_partition:Optional[float]=None) -> Tuple[float, float, float]:
         if dot_product is None:
             dot_product = np.dot(self.theta[node, :], node_values_suffst) - self.theta[node, node] * \
@@ -79,12 +65,6 @@ class SPGM(Model):
             datapoint_sf[ii] = self.sufficient_statistics(datapoint[ii])
 
         dot_product = np.dot(datapoint_sf, node_theta_curr) - node_theta_curr[node] * datapoint_sf[node] + node_theta_curr[node]
-        # if dot_product > 100:
-        #     print(node)
-        #     print(datapoint)
-        #     print(node_theta_curr)
-        #     print(dot_product)
-        #     print('')
         log_partition = np.exp(dot_product)
         ll = dot_product * datapoint_sf[node] - gammaln(datapoint[node]+1) - log_partition
 
@@ -93,7 +73,7 @@ class SPGM(Model):
     def calculate_grad_ll_datapoint(self, node:int, datapoint:np.ndarray, node_theta_curr:np.ndarray) -> np.ndarray:
         grad = np.zeros(datapoint.shape)
 
-        datapoint_sf = np.zeros((len(datapoint), ))
+        datapoint_sf = np.zeros((len(datapoint),))
         for ii in range(len(datapoint)):
             datapoint_sf[ii] = self.sufficient_statistics(datapoint[ii])
 
